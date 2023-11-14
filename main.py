93650e3818dbc912c357370fd31a7c53b606bb60
mmct13 <marshallchrist@yahoo.com>
2023-11-14 08:17:57 +0000

    App

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 import pandas as pd
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import ttk, filedialog, messagebox, simpledialog
+from PIL import Image, ImageTk
+
+def redimensionner_image(image_path, nouvelle_taille):
+    img = Image.open(image_path)
+    img_redimensionnee = img.resize(nouvelle_taille)
+    return ImageTk.PhotoImage(img_redimensionnee)
 
 def renommer_fichiers():
+    # ... (Votre fonction renommer_fichiers reste inchangée)
+
     # Fonction pour renommer les fichiers
 
     # Obtenez le chemin du dossier d'images
@@ -24,14 +32,30 @@ def renommer_fichiers():
         # Chargez le fichier Excel dans une dataframe pandas
         df_excel = pd.read_excel(fichier_excel)
 
-        # Vérifiez si la colonne 'seq' existe dans le DataFrame
-        if 'seq' in df_excel.columns:
-            # Convertissez la colonne 'seq' en chaînes de caractères
-            df_excel['seq'] = df_excel['seq'].astype(str)
+        # Vérifiez si la colonne 'refFournisseur' existe dans le DataFrame
+        if 'refFournisseur' in df_excel.columns:
+            # Convertissez la colonne 'refFournisseur' en chaînes de caractères
+            df_excel['refFournisseur'] = df_excel['refFournisseur'].astype(str)
+
+            # Barre de progression
+            progress_bar = ttk.Progressbar(fenetre, orient="horizontal", length=300, mode="determinate")
+            progress_bar.pack(pady=10)
+
+            # Widget Text pour les logs de la console
+            logs_text = tk.Text(fenetre, height=10, width=60)
+            logs_text.pack(pady=10)
+            logs_text.insert(tk.END, "Logs de la console :\n\n")
 
             # Parcourez tous les fichiers dans le dossier d'images
-            for fichier_image in os.listdir(dossier_images):
+            total_files = len([f for f in os.listdir(dossier_images) if os.path.isfile(os.path.join(dossier_images, f)) and f.lower().endswith(('.png', '.jpg', '.jpeg'))])
+            progress_bar["maximum"] = total_files
+
+            for i, fichier_image in enumerate(os.listdir(dossier_images)):
                 chemin_image = os.path.join(dossier_images, fichier_image)
+
+                # Mise à jour de la barre de progression
+                progress_bar["value"] = i + 1
+                fenetre.update_idletasks()
 
                 # Vérifiez si le fichier est un fichier image
                 if os.path.isfile(chemin_image) and fichier_image.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -43,51 +67,80 @@ def renommer_fichiers():
                         # Divisez le nom de fichier en deux parties (avant et après le tiret)
                         avant_tiret, apres_tiret = nom_fichier_sans_extension.split('-', 1)
 
-                        # Recherchez le nom dans la colonne 'seq' du fichier Excel
-                        correspondance = df_excel[df_excel['ref'] == avant_tiret]
+                        # Recherchez le nom dans la colonne 'refFournisseur' du fichier Excel
+                        correspondance = df_excel[df_excel['refFournisseur'] == avant_tiret]
 
                         # Si une correspondance est trouvée, renommez le fichier avec la partie après le tiret et ajoutez les tirets
                         if not correspondance.empty:
-                            nouvelle_valeur = correspondance.iloc[0]['ref']
+                            nouvelle_valeur = correspondance.iloc[0]['refArticle']
                             nouveau_nom = f"{nouvelle_valeur}-{apres_tiret}{extension}"  # Utilisez l'extension d'origine
                             nouveau_chemin = os.path.join(dossier_images, nouveau_nom)
 
                             # Renommez le fichier
                             os.rename(chemin_image, nouveau_chemin)
 
-                            print(f"Le fichier {fichier_image} a été renommé en {nouveau_nom}")
+                            log_message = f"Le fichier {fichier_image} a été renommé en {nouveau_nom}\n"
+                            logs_text.insert(tk.END, log_message)
+                            print(log_message)
                         else:
-                            print(f'Aucune correspondance pour {avant_tiret} !')
+                            log_message = f'Aucune correspondance pour {avant_tiret} !\n'
+                            logs_text.insert(tk.END, log_message)
+                            print(log_message)
                     else:
-                        # Si le fichier ne contient pas de tiret, renommez-le en utilisant la valeur dans la colonne 'seq' et ajoutez les tirets
-                        correspondance = df_excel[df_excel['seq'] == nom_fichier_sans_extension]
+                        # Si le fichier ne contient pas de tiret, renommez-le en utilisant la valeur dans la colonne 'refFournisseur' et ajoutez les tirets
+                        correspondance = df_excel[df_excel['refFournisseur'] == nom_fichier_sans_extension]
                         if not correspondance.empty:
-                            nouvelle_valeur = correspondance.iloc[0]['ref']
+                            nouvelle_valeur = correspondance.iloc[0]['refArticle']
                             nouveau_nom = f"{nouvelle_valeur}{extension}"  # Utilisez l'extension d'origine
                             nouveau_chemin = os.path.join(dossier_images, nouveau_nom)
 
                             # Renommez le fichier
                             os.rename(chemin_image, nouveau_chemin)
 
-                            print(f"Le fichier {fichier_image} a été renommé en {nouveau_nom}")
+                            log_message = f"Le fichier {fichier_image} a été renommé en {nouveau_nom}\n"
+                            logs_text.insert(tk.END, log_message)
+                            print(log_message)
                         else:
-                            print(f'Aucune correspondance pour {nom_fichier_sans_extension} !')
-
+                            log_message = f'Aucune correspondance pour {nom_fichier_sans_extension} !\n'
+                            logs_text.insert(tk.END, log_message)
+                            print(log_message)
 
             messagebox.showinfo("Opération terminée", "Le renommage des fichiers est terminé avec succès!")
 
         else:
-            messagebox.showwarning("Avertissement", "La colonne 'seq' n'a pas été trouvée dans le fichier Excel.")
+            messagebox.showwarning("Avertissement", "La colonne 'refFournisseur' n'a pas été trouvée dans le fichier Excel.")
 
     except Exception as e:
         messagebox.showerror("Erreur", f"Une erreur s'est produite : {str(e)}")
 
+    # Réinitialisez la barre de progression
+    progress_bar.destroy()
+    # Ajoutez un bouton OK pour fermer la fenêtre après l'opération
+    ok_button = tk.Button(fenetre, text="OK", command=fenetre.destroy)
+    ok_button.pack(pady=20)
+
 # Créez une fenêtre Tkinter
 fenetre = tk.Tk()
 fenetre.title("Renommer des fichiers")
-fenetre.geometry("720x360")
+fenetre.geometry("600x400")
+
+# Utilisez un thème moderne avec ttk
+style = ttk.Style()
+style.theme_use('clam')  # 'clam' est un thème moderne, vous pouvez choisir d'autres thèmes selon vos préférences
+
+# Ajoutez une icône personnalisée à la fenêtre (remplacez "chemin_vers_icone.ico" par le chemin de votre propre icône)
+icone_path = os.path.join(os.path.dirname(__file__), "logo_bernabee.ico")
+fenetre.wm_iconbitmap(icone_path, default=icone_path)
+
+# Redimensionnez l'image avant de l'afficher
+image_path = os.path.join(os.path.dirname(__file__), "logo_bernabee.png")
+nouvelle_taille = (250, 250)  # Spécifiez la nouvelle taille souhaitée
+image_redimensionnee = redimensionner_image(image_path, nouvelle_taille)
+label_image = ttk.Label(fenetre, image=image_redimensionnee)
+label_image.pack(pady=10)
+
 # Créez un bouton pour lancer l'opération
-bouton_lancer = tk.Button(fenetre, text="Lancer l'opération", command=renommer_fichiers)
+bouton_lancer = ttk.Button(fenetre, text="Lancer l'opération", command=renommer_fichiers)
 bouton_lancer.pack(pady=20)
 
 # Démarrez la boucle principale Tkinter
